8563f89f73a6b7c0cdbbee6715f683b783584b4a
Orlandoseoanes <oaseoanes@unicesar.edu.co>
2023-06-01 17:20:32 -0500

    primer commit

---
+++ src/database/sequelize.config.js
@@ -0,0 +1,18 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+const { Sequelize } = require("sequelize");
+
+// const sequelize = new Sequelize("tic-tac-toe-api", "apiuser", "password", {
+//   host: "192.168.1.12",
+//   dialect: "mysql",
+// });
+//const sequelize = new Sequelize(process.env.DBNAME, process.env.DBUSER, process.env.DBPASS, {
+ // host: process.env.DBHOST,
+//dialect: "mysql",
+//});
+
+const sequelize = new Sequelize(process.env.DBURL) // Example for postgres
+
+
+module.exports = { sequelize };
